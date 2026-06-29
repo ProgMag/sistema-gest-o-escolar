f8d864c0704a5deefaedb693ab580170df2dbc5d
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-06-29 18:28:06 -0300

    Adiciona método aplicarReajuste

--- src/Professor.java
+++ src/Professor.java
@@ -6,4 +6,12 @@ public class Professor extends Pessoa {
         super(nome, cpf, idade);
         this.especialidade = especialidade;
     }
+
+    public double aplicarReajuste(double percentual) {
+        if (percentual <= 0) {
+            System.out.println("O reajuste precisa ser maior que 0");
+        } else {
+            return
+        }
+    }
 }
