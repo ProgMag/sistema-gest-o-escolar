1a92a4124d6041bbe9a234159a0e5c56754365b1
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-06-29 17:54:34 -0300

    Adiciona construtor para a classe Pessao

---
+++ src/Pessoa.java
@@ -0,0 +1,17 @@
+public class Pessoa {
+    private String nome;
+    private String cpf;
+    private int idade;
+
+    public Pessoa(String nome, String cpf, int idade) {
+        this.nome = nome;
+        this.cpf = cpf;
+
+        if (idade > 0) {
+            this.idade = idade;
+        } else {
+            this.idade = 1;
+            System.out.println("Idade inválida. Definida como 1");
+        }
+    }
+}
