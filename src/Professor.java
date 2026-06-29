0ab8928f4346d61716d221c7d07c00dce8335a23
Guilherme Queiroz <estudosprogrmacao@gmail.com>
2026-06-29 18:08:29 -0300

    Adiciona construtor na classe Professor

---
+++ src/Professor.java
@@ -0,0 +1,9 @@
+public class Professor extends Pessoa {
+
+    private String especialidade;
+
+    public Professor(String nome, String cpf, int idade) {
+        super(nome, cpf, idade);
+        this.especialidade = especialidade;
+    }
+}
